77d2eef15b6d2d14973672ad69bc7d66f1ce557e
minjunsz <minjunsz@postech.ac.kr>
2023-01-17 16:17:16 +0900

    feat(type): impl. rdkit type stub

---
+++ typings/rdkit-stubs/Geometry/__init__.pyi
@@ -0,0 +1,1 @@
+from rdkit.Geometry.rdGeometry import *
---
+++ typings/rdkit-stubs/RDConfig.pyi
@@ -0,0 +1,8 @@
+# pylint: disable=all
+
+RDCodeDir: str
+RDContribDir: str
+RDDataDatabase: str
+RDDataDir: str
+RDDocsDir: str
+RDProjDir: str
